c3ad1bc995e693753861f015d88b531c4de90771
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2018-11-25 17:35:25 +0800

    Ci

--- ci/before_install.sh
+++ ci/before_install.sh
@@ -3,4 +3,4 @@
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt-get update
 sudo apt-get install shellcheck python3.6 -y
-sudo pip install vim-vint pathlib typing enum unique
+sudo pip install vim-vint pathlib typing enum34
