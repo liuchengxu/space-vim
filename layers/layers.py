fcdfd97528d67b15634f10a470b83b504e008bd0
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2019-02-07 19:50:00 +0800

    Sort plugins in LAYERS.md
    Remove linecache

--- layers/layers.py
+++ layers/layers.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 
@@ -23,7 +23,8 @@ with open(info_path) as f:
     info_topics = ast.literal_eval(lines[0].split('=')[1].strip())
     info_layers = ast.literal_eval(lines[1].split('=')[1].strip())
 
-output = open(os.path.expandvars('./LAYERS.md'), 'w')
+layers_md = os.path.expandvars('./LAYERS.md')
+output = open(layers_md, 'w')
 
 header = []
 header.append('Layer Manifest\n')
@@ -31,8 +32,7 @@ header.append('==============\n\n')
 header.append('Last updated: ' +
               time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
 header.append("\n\n")
-header.append(
-    "Default layers: `fzf`, `unite`, `better-defaults` and `which-key`.")
+header.append("Default layers: `fzf`, `better-defaults` and `which-key`.")
 header.append("\n\n")
 
 header.append("%-20s | %s | %s\n" % ('Topic', 'Layer', 'Plugins'))
@@ -44,10 +44,11 @@ url_prefix = 'https://github.com/liuchengxu/space-vim/tree/master/layers'
 
 plugins = []
 
-for topic, layers in info_topics.items():
+for topic in sorted(info_topics.keys()):
+    layers = info_topics[topic]
     pattern = re.compile('\'(.*)\'')
-    for layer in layers:
-        plugs = "<ul>"
+    for layer in sorted(layers):
+        plugs = []
         with open(info_layers[layer]['dir'] + '/packages.vim') as f:
             for line in f.read().splitlines():
                 if line.startswith('"'):
@@ -62,12 +63,16 @@ for topic, layers in info_topics.items():
                             continue
                         else:
                             plugins.append(plug)
-                            plugs += (
-                                "<li>[{}](https://github.com/{})</li>".format(
-                                    plug, plug))
-        plugs += "</ul>"
-        output.write("%-20s | [%s](%s/%s/%s) | %s\n" %
-                     (topic, layer, url_prefix, topic, layer, plugs))
+                            plugs.append(plug)
+        plugs.sort()
+        plugs = map(
+            lambda x: "<li>[{}](https://github.com/{})</li>".format(x, x),
+            plugs)
+        plugs = '<ul>' + ''.join(plugs) + '</ul>'
+        row = ("%-20s | [%s](%s/%s/%s) | %s\n" % (topic, layer, url_prefix,
+                                                  topic, layer, plugs))
+        output.write(row)
+
 output.close()
 
 print('LAYERS.md has been updated (created) successfully.')
